6baae479728da25cc24f0d6d4459a16cbbe55d40
Gagan <gaganichake@yahoo.com>
2022-04-20 11:41:06 -0500

    Using useRef and Vanilla JavaScript

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useRef } from "react";
+
+
 const SimpleInput = (props) => {
+
+  const nameInputRef = useRef();
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue); // Vanilla JavaScript
+    nameInputRef.current.value = ''; // Not ideal, don't manipulate the DOM directly.
+  }
+  
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' />
       </div>
       <div className="form-actions">
         <button>Submit</button>
