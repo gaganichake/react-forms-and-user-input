ab5c11b52e80012ed8c3d3cd42ffd09eacdf4d7a
Gagan <gaganichake@yahoo.com>
2022-04-20 12:04:27 -0500

    Adding Basic Validation and Providing Validation Feedback

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value); // Handling every keystroke
@@ -10,6 +11,12 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    if(enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return
+    }
+    setEnteredNameIsValid(true);
 
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue); // Vanilla JavaScript
@@ -19,12 +26,15 @@ const SimpleInput = (props) => {
     setEnteredName('');
   };
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'; 
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input ref={nameInputRef} onChange={nameInputChangeHandler} type="text" id="name" />
       </div>
+      {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
