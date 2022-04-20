a06cf6130f17d9f4a357a5cb16a6af3572d8ca23
Gagan <gaganichake@yahoo.com>
2022-04-20 11:46:52 -0500

    Using useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,23 +1,29 @@
-import { useRef } from "react";
-
+import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-
   const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value); // Handling every keystroke
+  }
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue); // Vanilla JavaScript
-    nameInputRef.current.value = ''; // Not ideal, don't manipulate the DOM directly.
-  }
-  
+    nameInputRef.current.value = ""; // Not ideal, don't manipulate the DOM directly.
+
+    console.log(enteredName);
+    setEnteredName('');
+  };
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input ref={nameInputRef} onChange={nameInputChangeHandler} type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
